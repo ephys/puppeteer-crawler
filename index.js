8c61bd61a1606b1c6aa5f30b313ba3d4be3fe98f
Guylian Cox <hello@guylian.me>
2020-08-14 12:36:56 +0200

    feat: collect resources & lighthouse score

--- index.js
+++ index.js
@@ -2,9 +2,27 @@
 'use strict';
 
 const fs = require('fs');
+const crypto = require('crypto');
 const path = require('path');
+const lighthouse = require('lighthouse');
 const puppeteer = require('puppeteer');
 const meow = require('meow');
+
+// TODO: respect noindex/nofollow if flag is set
+// TODO: use sitemap.xml
+// TODO: collect robots.txt
+// TODO: handle rel=alternate
+// TODO: handle rel=canonical
+// TODO: disable auto-redirect & log redirection chains - https://github.com/puppeteer/puppeteer/issues/1132
+// TODO: if a page has the same MD5 as another, tag them as "potential canonicals" & warn if they're not tagged as canonicals of one-another
+// TODO: run lighthouse mobile + desktop
+// TODO: check if #anchor exists?
+// TODO: remove anchor when requesting a page?
+// TODO: normalize trailing /
+
+function md4(text) {
+  return crypto.createHash('md4').update(text).digest('hex');
+}
 
 const cliName = path.relative(process.cwd(), process.argv[1]);
 
@@ -16,6 +34,7 @@ const cli = meow(`
 	  --domain-alias <url>  Add an extra domain that the scraper can consider as being the same domain as the base url.
 	  --collect-meta        Collect SEO metadata (title, description, etc) of crawled webpages.
 	  --delay <delay>       Pause time between two page fetches in ms (default to 500ms).
+	  --lighthouse          Run lighthouse on crawled pages.
 
 	Examples
 	  $ ${cliName} https://wellmade.be --domain-alias https://www.wellmade.be
@@ -32,6 +51,10 @@ const cli = meow(`
       type: 'string',
       inferType: true,
       default: '500',
+    },
+    'lighthouse': {
+      type: 'boolean',
+      default: false,
     },
   }
 });
@@ -50,6 +73,7 @@ let browser;
 
   const delay = Number(cli.flags.delay);
   const collectSeo = cli.flags.collectMeta || false;
+  const collectLighthouse = cli.flags.lighthouse || false;
   const validDomains = !cli.flags.domainAlias
     ? []
     : Array.isArray(cli.flags.domainAlias)
@@ -96,6 +120,9 @@ let browser;
     }
   }
 
+  // reduce requests
+  // await page.setRequestInterception(true);
+
   normalizeUrlSet(visitedUrls, canonicalHost);
   normalizeUrlSet(pendingUrls, canonicalHost);
   // we don't normalize external urls as we can't assume anything about other websites
@@ -104,71 +131,109 @@ let browser;
     await sleep(delay);
 
     const visitingUrl = pendingUrls.values().next().value;
+    const pageMeta = {};
 
     console.log('[VISITING]', visitingUrl);
 
+    const logRequests = request => {
+      /*
+       document, stylesheet, image, media, font, script, texttrack, xhr, fetch, eventsource, websocket, manifest, other
+       */
+
+      if (collectSeo) {
+        const url = request.url();
+        const type = request.resourceType();
+
+        if (['image', 'stylesheet', 'font', 'media', 'script'].includes(type)) {
+          pageMeta[type] = pageMeta[type] || [];
+
+          pageMeta[type].push(url);
+        } else {
+          console.log('Untracked request', type, url);
+        }
+      }
+    };
+
     try {
-      const response = await page.goto(visitingUrl);
-      if (!response.ok()) {
-        console.error('[COULD NOT NAVIGATE]', visitingUrl, `(${response.status()})`);
+      page.on('request', logRequests);
+
+      try {
+        // if we collect SEO: we wait for page to be fully loaded as we collect the list of referenced resources
+        const response = await page.goto(visitingUrl, collectSeo ? { waitUntil: 'networkidle2' } : undefined);
+        if (!response.ok()) {
+          console.error('[COULD NOT NAVIGATE]', visitingUrl, `(${response.status()})`);
+          unreachableUrls.add(visitingUrl);
+          pendingUrls.delete(visitingUrl);
+          continue;
+        }
+      } catch (e) {
+        console.error('[COULD NOT NAVIGATE]', visitingUrl, `(${e.message})`);
         unreachableUrls.add(visitingUrl);
         pendingUrls.delete(visitingUrl);
         continue;
       }
-    } catch (e) {
-      console.error('[COULD NOT NAVIGATE]', visitingUrl, `(${e.message})`);
-      unreachableUrls.add(visitingUrl);
+
+      const { anchors, meta: seoMeta } = await page.evaluate(() => {
+        const anchors = Array.from(document.querySelectorAll('a')).map(anchor => anchor.href);
+
+        const metaTags = document.querySelectorAll('meta[name="robots"], meta[name="description"], meta[name="keywords"], meta[property^="og:"], meta[name^="twitter:"]');
+        const metaMapping = {};
+        for (const metaTag of metaTags) {
+          const name = metaTag.getAttribute('name') || metaTag.getAttribute('property');
+          metaMapping[name] = metaTag.content;
+        }
+
+        const meta = {
+          title: document.title,
+          ...metaMapping,
+        };
+
+        return {
+          anchors,
+          meta,
+        };
+      });
+
+      if (collectSeo) {
+        const pageHash = md4(await page.content());
+        let lighthouseScore;
+        if (collectLighthouse) {
+          lighthouseScore = await runLighthouse(browser, visitingUrl);
+        }
+
+        seoState[visitingUrl] = Object.assign(pageMeta, seoMeta, {
+          anchors,
+          lighthouse: lighthouseScore,
+          hash: pageHash,
+        });
+
+        // seoState[visitingUrl].microdata = await evaluateMicrodata(page);
+        saveState(seoFile, seoState);
+      }
+
+      for (const url of anchors) {
+
+        if (!url) {
+          continue;
+        }
+
+        const normalized = normalizeUrl(url, canonicalHost);
+
+        if (!isSameDomain(validDomains, url)) {
+          externalUrls.add(url);
+        } else if (!visitedUrls.has(normalized) && !pendingUrls.has(normalized)) {
+          pendingUrls.add(normalized);
+          console.log('[DISCOVERED]', normalized);
+        }
+      }
+
+      visitedUrls.add(visitingUrl);
       pendingUrls.delete(visitingUrl);
-      continue;
+
+      saveState(stateFile, { visitedUrls, pendingUrls, externalUrls, unreachableUrls });
+    } finally {
+      page.off('request', logRequests);
     }
-
-    const { anchors, meta } = await page.evaluate(() => {
-      const anchors = Array.from(document.querySelectorAll('a')).map(anchor => anchor.href);
-
-      const metaTags = document.querySelectorAll('meta[name="robots"], meta[name="description"], meta[name="keywords"], meta[property^="og:"], meta[name^="twitter:"]');
-      const metaMapping = {};
-      for (const metaTag of metaTags) {
-        const name = metaTag.getAttribute('name') || metaTag.getAttribute('property');
-        metaMapping[name] = metaTag.content;
-      }
-
-      const meta = {
-        title: document.title,
-        ...metaMapping,
-      };
-
-      return {
-        anchors,
-        meta,
-      };
-    });
-
-    if (collectSeo) {
-      seoState[visitingUrl] = meta;
-      // seoState[visitingUrl].microdata = await evaluateMicrodata(page);
-      saveState(seoFile, seoState);
-    }
-
-    for (const url of anchors) {
-
-      if (!url) {
-        continue;
-      }
-
-      const normalized = normalizeUrl(url, canonicalHost);
-
-      if (!isSameDomain(validDomains, url)) {
-        externalUrls.add(url);
-      } else if (!visitedUrls.has(normalized) && !pendingUrls.has(normalized)) {
-        pendingUrls.add(normalized);
-        console.log('[DISCOVERED]', normalized);
-      }
-    }
-
-    visitedUrls.add(visitingUrl);
-    pendingUrls.delete(visitingUrl);
-
-    saveState(stateFile, { visitedUrls, pendingUrls, externalUrls, unreachableUrls });
   }
 })().finally(() => {
   return browser && browser.close();
@@ -198,7 +263,7 @@ function normalizeUrl(urlStr, canonicalHost) {
   // TODO option --include-hash
   // url.hash === '' because if url ends with #, hash is '' instead of '#'
   // if (url.hash === '' || url.hash === '#' || url.hash === '#null') {
-  url.hash = '';
+  url.md4 = '';
   // }
 
   return url.toString();
@@ -405,4 +470,49 @@ function evaluateMicrodata(page) {
 
     return parseMicrodata(document);
   });
+}
+
+async function runLighthouse(browser, url) {
+  // Wait for Lighthouse to open url, then customize network conditions.
+// Note: this will re-establish these conditions when LH reloads the page. Think that's ok....
+  browser.on('targetchanged', async target => {
+    const page = await target.page();
+
+    function addStyleContent(content) {
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      style.appendChild(document.createTextNode(content));
+      document.head.appendChild(style);
+    }
+
+    const css = '* {color: red}';
+
+    if (page && page.url() === url) {
+      // Note: can't use page.addStyleTag due to github.com/GoogleChrome/puppeteer/issues/1955.
+      // Do it ourselves.
+      const client = await page.target().createCDPSession();
+      await client.send('Runtime.evaluate', {
+        expression: `(${addStyleContent.toString()})('${css}')`
+      });
+    }
+  });
+
+// Lighthouse will open URL. Puppeteer observes `targetchanged` and sets up network conditions.
+// Possible race condition.
+  const {lhr} = await lighthouse(url, {
+    port: (new URL(browser.wsEndpoint())).port,
+    onlyCategories: ['performance', 'accessibility', 'best-practices', 'seo', 'pwa'],
+    output: 'json',
+    logLevel: 'error',
+  });
+
+  // console.log('Report is done for', lhr.finalUrl);
+  // // console.log(lhr);
+  // console.log(`Lighthouse scores: ${Object.entries(lhr.categories).map(([key, result]) => {
+  //   return `${key}: ${result.score}`;
+  // }).join(', ')}`);
+
+  return Object.fromEntries(Object.entries(lhr.categories).map(([key, result]) => {
+    return [key, result.score];
+  }));
 }
